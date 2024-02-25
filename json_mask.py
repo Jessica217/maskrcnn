f0ed83b65b36765e53c0b87260a22bd197c9d454
yaya <2439070462@qq.com>
2024-02-25 22:16:32 +0800

    提交修改mask的文件

---
+++ json_mask.py
@@ -0,0 +1,32 @@
+import numpy as np
+import json
+import cv2
+import os
+import glob
+
+def get_file_pos(folder_path):
+    json_files = glob.glob(os.path.join(folder_path, '*.json'))
+    for json_file in json_files:
+        print(json_file)
+        # json_path = './datasets/extra_DMSA_VAL/RGB_mode/0001.json'
+        with open(json_file, 'r') as f:
+            points = json.load(f)
+            # print(points["imageHeight"], points["imageWidth"])
+            left_kidney_pos = points["shapes"][0]["points"]
+            right_kidney_pos = points["shapes"][1]["points"]
+
+        img = np.zeros((points["imageHeight"], points["imageWidth"], 3), dtype = np.uint8)# 使用cv2创建与原图尺寸相同的图像
+        pts1 = np.array(left_kidney_pos, np.int32)
+        pts2 = np.array(right_kidney_pos, np.int32)
+        cv2.fillPoly(img, [pts1], color = (255, 255, 255))
+        cv2.fillPoly(img, [pts2], color = (255, 255, 255))
+        # print(img)
+        masks = np.where(img == 255, True, False)
+        print(masks.shape)
+        print(np.sum(masks == True))
+        cv2.imshow('img', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+# get_file_pos(folder_path = './datasets/extra_DMSA_VAL/RGB_mode/')
+
